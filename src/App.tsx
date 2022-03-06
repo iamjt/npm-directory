83aa376e68eaecf176a41b19acb83082df1d9f14
Jing Ting <khoojt@gmail.com>
2022-03-07 01:01:24 +0800

    Added function to fetch data from NPM

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const NPM_REGISTRY = "https://registry.npmjs.org/-/v1/search?text=";
+
+class App extends React.Component {
+  
+  constructor(props) {
+    super (props);
+  }
+
+  componentDidMount() {
+    this.performSearch();
+  }
+
+  //Similar to the shortlisting above
+  //But this function will handle results of N sizes
+  //Default search result size is 100 because we need to do pagination
+  performSearch() {
+
+    const testSearchVal = "React";
+
+    //Deal with empty input
+    let searchURL;
+    if(!testSearchVal || testSearchVal.trim().length == 0) {
+      return;
+    } else {
+      //Search for the desired terms and set the default search size to 100
+      searchURL = NPM_REGISTRY+testSearchVal.trim();
+    }
+
+    fetch(searchURL).then(searchResponse => {
+      if(searchResponse.ok) {
+        searchResponse.json().then(data => {
+          console.log(data);          
+        });
+      } else {
+        //Error Handling
+      }    
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+
 }
 
 export default App;
